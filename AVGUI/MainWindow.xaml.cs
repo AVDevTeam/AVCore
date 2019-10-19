814151788f5df3dd15fdc49b250ec9ca5a5f6db3
darsigovrustam <darsigov.rustam@yandex.ru>
2019-10-19 14:54:01 +0300

    GUI 2 buttons were added

--- AVGUI/MainWindow.xaml.cs
+++ AVGUI/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.IO.Pipes;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace AVGUI
 {
@@ -20,11 +19,41 @@ namespace AVGUI
         {
             Loaded -= Main;
 
-            PipeClient pipe = new PipeClient("AVCorePipe");
-            pipe.Connect();
+            //PipeClient pipe = new PipeClient("AVCorePipe");
+            //pipe.Connect();
 
-            string message = pipe.ReciveMessage();
-            pipe.SendMessage("HELLO");
+            //string message = pipe.ReciveMessage();
+            //pipe.SendMessage("HELLO");
+        }
+
+        private void ScanModeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((Slider)sender).SelectionEnd = e.NewValue;
+
+            switch (ScanModeSlider.SelectionEnd)
+            {
+                case 0:
+                    ScanButtonTextBlock.Text = "ПОЛНОЕ СКАНИРОВАНИЕ";
+                    break;
+                case 1:
+                    ScanButtonTextBlock.Text = "БЫСТРОЕ СКАНИРОВАНИЕ";
+                    break;
+                case 2:
+                    ScanButtonTextBlock.Text = "ВЫБОРОЧНОЕ СКАНИРОВАНИЕ";
+                    break;
+            }
+
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.ShowDialog();
+        }
+
+        private void ScanButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
